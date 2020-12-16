bb8b73170395d4bde6c881a015a3face0bd974d8
deliahellander <70303141+deliahellander@users.noreply.github.com>
2020-12-16 18:04:11 -0500

    working on datetime filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,10 +4,10 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log the UFO Sighting data from data.js
 console.log(data);
 
-// YOUR CODE HERE!
+// loop through data array and append rows to javascript table
 data.forEach(function(UFOSighting) {
     console.log(UFOSighting);
     var row = tbody.append("tr");
@@ -16,3 +16,23 @@ data.forEach(function(UFOSighting) {
         console.log(key, value);
     });
 });
+// Getting a reference to the button on the page
+var button = d3.select("filter-btn");
+
+// Getting a reference to the input element on the page 
+var inputField = d3.select("datetime");
+
+function handleClick() {
+    // We can use d3 to see the object that dispatched the event
+    console.log(d3.event.target);
+    d3.event.preventDefault();
+    console.log(inputField.property("value"));
+    var filtered_table = tableData.filter(UFOSighting => UFOSighting.datetime===inputField.property("value"))
+    displayData(filtered_table)
+
+};
+button.on("click", handleClick)
+
+// Input fields can trigger a change event when new text is entered.
+inputField.on("change", handleClick)
+  
