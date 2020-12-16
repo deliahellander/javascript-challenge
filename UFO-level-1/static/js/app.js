939250857a91ff5ed9b3519cad49b99a715d1e81
deliahellander <70303141+deliahellander@users.noreply.github.com>
2020-12-16 17:24:25 -0500

    automated table created

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
 // YOUR CODE HERE!
+data.forEach(function(UFOSighting) {
+    console.log(UFOSighting);
+    var row = tbody.append("tr");
+    Object.entries(UFOSighting).forEach(function([key, value]){
+        row.append("td").text(value);
+        console.log(key, value);
+    });
+});
